deda144557155d18cfe8a0a1f3590c2f3131ca27
abjbasil <a.j.basil@hotmail.com>
2025-05-12 19:44:13 +0100

    Top Paying Job Skills Related

---
+++ project_sql/2_Top_Paying_Job_Skills.sql
@@ -0,0 +1,15 @@
+SELECT 
+job_id,
+job_title_short,
+name AS company_name,
+salary_year_avg
+FROM job_postings_fact
+LEFT JOIN company_dim ON
+job_postings_fact.company_id = company_dim.company_id
+WHERE 
+job_title_short = 'Data Analyst' AND
+salary_year_avg IS NOT NULL AND
+job_location ='Anywhere'
+ORDER BY salary_year_avg DESC
+LIMIT 10
+-- not finished
