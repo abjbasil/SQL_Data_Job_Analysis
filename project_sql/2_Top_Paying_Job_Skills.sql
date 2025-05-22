0b8d391bfe2ddc191ae07e6d3c7ff379bdfe92f5
abjbasil <a.j.basil@hotmail.com>
2025-05-22 12:27:24 +0100

    Top Paying Job Skills Inner Joins & CTE

--- project_sql/2_Top_Paying_Job_Skills.sql
+++ project_sql/2_Top_Paying_Job_Skills.sql
@@ -1,3 +1,4 @@
+WITH top_paying_jobs AS (
 SELECT 
 job_id,
 job_title_short,
@@ -12,4 +13,17 @@ salary_year_avg IS NOT NULL AND
 job_location ='Anywhere'
 ORDER BY salary_year_avg DESC
 LIMIT 10
--- not finished
+)
+SELECT 
+top_paying_jobs.*,
+skills
+FROM top_paying_jobs
+ INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+ INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ ORDER BY salary_year_avg DESC
+
+ /* Based on job postings, the following is a breakdown of the most in-demand competencies for data analysts in 2023:
+• With a bold count of eight, SQL is in the lead.
+• With a bold count of 7, Python comes in second.
+• With a bold count of six, Tableau is also very popular.
+• The demand for other skills, including as R, Snowflake, Pandas, and Excel, varies.*/
